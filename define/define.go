fe96309eb4e3b9045406a9fe92f3da2078bc8039
FanYaNan <64038944@qq.com>
2022-05-23 17:43:40 +0800

    修改常量

--- define/define.go
+++ define/define.go
@@ -2,7 +2,6 @@ package define
 
 import (
 	"github.com/dgrijalva/jwt-go"
-	"os"
 )
 
 type UserClaim struct {
@@ -18,11 +17,6 @@ var CodeLength = 6
 
 //验证码过器时间
 var CodeExpire = 300
-
-//腾讯云COS对象存储配置
-var TencentSecretId = os.Getenv("AKIDf7IDtIRM1RebHkvpRloZ3FFyPUhja9QP")
-var TencentSecretKey = os.Getenv("j9GdjY5RVIn0vd3BxRfdMnxpPhhPqyoN")
-var CosBucketDomain = "https://net-disk-1257185745.cos.ap-beijing.myqcloud.com"
 
 //分页默认参数
 var PageSize = 20
